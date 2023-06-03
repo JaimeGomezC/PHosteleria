b2de88e8d07a6bac3acdbbdc6bc09fa48cb06a76
SEBASTIAN FERNANDEZ SAEZ <sfs50p@carm.es>
2023-06-03 15:25:28 +0200

    Cambiar idioma v.1

--- frontend/src/app/componentes/navbar/navbar.component.ts
+++ frontend/src/app/componentes/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TraductorService } from 'src/app/servicios/traductor.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private traductorService: TraductorService) { }
+  public tr=this.traductorService;
+  
   ngOnInit(): void {
+    this.tr.loadTranslations()
+      .then(() => {
+        // Las traducciones se han cargado correctamente
+        console.dir(this.tr);
+
+        // Puedes realizar otras acciones después de cargar las traducciones si es necesario
+      })
+      .catch(error => {
+        // Hubo un error al cargar las traducciones
+        console.error('Error loading translations:', error);
+      });
+  }
+
+  cambiarIdioma(idioma: any): void {
+    this.tr.cambiarIdioma(idioma.value);
   }
 
 }
